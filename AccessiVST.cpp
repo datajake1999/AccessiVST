83873b5f90993e686f0ca8bcb3d6eb8ed745f77d
datajake1999 <datajake@gmail.com>
2021-10-20 11:24:44 -0700

    Fixed casing of include statement.

--- AccessiVST.cpp
+++ AccessiVST.cpp
@@ -17,7 +17,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-#include "accessivst.h"
+#include "AccessiVST.h"
 #include <stdio.h>
 #ifdef _win32
 #include <windows.h>
